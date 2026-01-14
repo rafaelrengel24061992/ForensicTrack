0a93fa0f31c1998fa1a012a43d7440f54137bf3f
rafaelrengel24061992 <rafaelrengel.rr@gmail.com>
2026-01-14 14:11:32 -0300

    FINAL 6.0

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,10 +1,11 @@
 import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { app } from './firebase'; 
 
-// Inicializa o Auth usando a instância do app exportada
 export const auth = getAuth(app);
 
-export const login = (email: string, pass: string) => 
-  signInWithEmailAndPassword(auth, email, pass);
+export const login = async (email: string, pass: string) => {
+  // Usamos o retorno direto do Firebase
+  return await signInWithEmailAndPassword(auth, email, pass);
+};
 
 export const logout = () => signOut(auth);
